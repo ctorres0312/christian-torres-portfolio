1da753a2eb86c00a803ee68a65c08f76f6be2833
ctorres0312 <113136320+ctorres0312@users.noreply.github.com>
2023-11-03 00:00:57 -0400

    logo update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
-      
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
-
       
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
