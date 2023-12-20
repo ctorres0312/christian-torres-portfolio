07ebd9fa3b026b498e472819ff60f0b7f5af8ddb
ctorres0312 <113136320+ctorres0312@users.noreply.github.com>
2023-12-20 17:40:39 -0500

    added number guessing game project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,72 +1,113 @@
-import React from 'react'
-import ThriftThreads from '../assets/THRIFTHREADS.png'
-import Mixifi from '../assets/MIXIFI.png'
-
+import React from "react";
+import ThriftThreads from "../assets/THRIFTHREADS.png";
+import Mixifi from "../assets/MIXIFI.png";
+import GuessNumber from "../assets/guess-a-number.png";
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#80D1FF]'>Work</p>
-            <p className='py-6'>// Check out some of my recent work</p>
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#80D1FF]">
+            Work
+          </p>
+          <p className="py-6">// Check out some of my recent work</p>
         </div>
 
-        <div 
-      
-              //  container
-        className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+        <div
+          //  container
+          className="grid sm:grid-cols-2 md:grid-cols-3 gap-4"
+        >
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${ThriftThreads})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
 
-
-           {/* Grid Item */}
-            <div 
-            style={{backgroundImage: `url(${ThriftThreads})`}}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-            >
-                {/* Hover effects */}
-                <div className='opacity-0 group group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                React JS Application
-                            </span>
-
-                        <div className='pt-8 text-center'>
-                            <a target="_blank" href='https://thriftthreads.vercel.app/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' >Demo</button>
-                            </a>
-                            <a target="_blank" href='https://github.com/ctorres0312/Thrift-Threads'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' >Code</button>
-                            </a>
-                        </div>
-                  </div>
-                </div>
-                  
-                  {/* Grid Item */}
-                <div 
-            style={{backgroundImage: `url(${Mixifi})`}}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-            >
-                {/* Hover effects */}
-                <div className='opacity-0 group group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                JS Application
-                            </span>
-
-                        <div className='pt-8 text-center'>
-                            <a target="_blank" href='https://mixifi.vercel.app/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' >Demo</button>
-                            </a>
-                            <a target="_blank" href='https://github.com/ctorres0312/Mixifi'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' >Code</button>
-                            </a>
-                        </div>
-                  </div>
-                </div>
-
-
+              <div className="pt-8 text-center">
+                <a target="_blank" href="https://thriftthreads.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  target="_blank"
+                  href="https://github.com/ctorres0312/Thrift-Threads"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
             </div>
+          </div>
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${GuessNumber})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                JS Application
+              </span>
+
+              <div className="pt-8 text-center">
+                <a
+                  target="_blank"
+                  href="https://number-guessing-game-ct.vercel.app/"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  target="_blank"
+                  href="https://github.com/ctorres0312/Number-Guessing-Game"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Mixifi})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                JS Application
+              </span>
+
+              <div className="pt-8 text-center">
+                <a target="_blank" href="https://mixifi.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a target="_blank" href="https://github.com/ctorres0312/Mixifi">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
